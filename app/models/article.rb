dcc52263d858c4b135ccf0acf1ae48ccc6a74531
Adom Hartell <adomhartell@gmail.com>
2016-07-17 21:34:02 +0000

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, :through => :taggings
+  
+  def tag_list
+    tags.join(", ")
+  end
+  
+  def tag_list=(string)
+    tag_names = string.split(",").collect{|s| s.strip.downcase}.uniq
+    tag_objects = tag_names.collect { |tag_name| Tag.find_or_create_by(name: tag_name) }
+    self.tags = tag_objects
+  end
 end
