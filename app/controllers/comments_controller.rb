c1a9849ac9c1925da5947a4d1b36139583445b8e
4t0m <adomhartell@gmail.com>
2016-07-22 23:41:48 -0400

    Sorcery authentication complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,17 @@
 class CommentsController < ApplicationController
   include CommentsHelper
+  before_filter :require_login, except: [:create]
+
 
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
     @comment.save
-    
-    
+
+
     flash.notice = "Comment Created!"
-    
+
     redirect_to article_path(@comment.article)
   end
-  
+
 end
